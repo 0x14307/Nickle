c6f77facdd1b71523be08ec7c53d8f90c890e1ba
Yaya48 <jemefaitchierdu65@gmail.com>
2021-02-19 17:00:44 +0100

    Error handler.

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const Client = require("./class/Client"),
 // Handlers Setup.
 ["cmd", "event"].forEach(handler => {
 
-    require(`./handlers/${handler}`)(client);
-    console.log(`[Handlers Setup] : Loaded Handlers : ${handler}`)
-
+    try {
+        require(`./handlers/${handler}`)(client);
+        console.log(`[Handlers Setup] : Loaded Handlers : ${handler}`)
+    } catch (e) {
+        return console.log(`[Handlers Setup] : ${handler} failed to load : ${e}`)
+    }
 
 });
 client.login(client.config.TOKEN);
